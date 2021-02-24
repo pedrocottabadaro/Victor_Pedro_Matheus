8b33bc05baf6e3316301e8974d7f233c2fead2b2
VictorGuerrah <38054544+VictorGuerrah@users.noreply.github.com>
2021-02-23 22:42:24 -0300

    Alterando tela de exercicio selecionado

---
+++ frontend/exercicioSelecionado/exercicioSelecionado.js
@@ -0,0 +1,14 @@
+function sair(){
+    window.location.href='../login/tela-login.html'
+  }
+  
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
+  
+  
